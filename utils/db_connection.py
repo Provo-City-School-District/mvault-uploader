5c95b89f916d8c1a37f76f523e5e49c22e2a091d
Josh Espinoza <joshespi@gmail.com>
2025-02-06 12:45:40 -0700

    DB connection utility

---
+++ utils/db_connection.py
@@ -0,0 +1,39 @@
+import mariadb
+import sys
+
+class DatabaseConnection:
+    def __init__(self, host, user, password, database, port=3306):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.conn = mariadb.connect(
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                database=self.database
+            )
+            self.cursor = self.conn.cursor()
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+
+    def execute_query(self, query, params):
+        try:
+            self.cursor.execute(query, params)
+            self.conn.commit()
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
